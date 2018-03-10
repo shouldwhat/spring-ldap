36a0ec7076f3d73c150edc1a2e41beccde873e39
wkkim <wonkkyuu@gmail.com>
2018-03-10 15:26:19 +0900

    Init project

---
+++ src/main/java/local/wkim/ldap/AboutLdapApplication.java
@@ -0,0 +1,12 @@
+package local.wkim.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AboutLdapApplication.class, args);
+	}
+}
