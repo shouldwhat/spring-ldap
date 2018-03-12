93ba0b398bc71d70d31a07af4f4329624b345f64
wkkim <wonkkyuu@gmail.com>
2018-03-12 18:44:44 +0900

    manager 클래스 누락 코드 추가

--- src/main/java/local/wkim/ldap/manager/LdapComputerManager.java
+++ src/main/java/local/wkim/ldap/manager/LdapComputerManager.java
@@ -3,8 +3,11 @@ package local.wkim.ldap.manager;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import local.wkim.ldap.entity.LdapComputer;
+import local.wkim.ldap.manager.base.LdapManager;
+
 @Scope("prototype")
 @Component("computer")
-public class LdapComputerManager {
+public class LdapComputerManager extends LdapManager<LdapComputer> {
 
 }
