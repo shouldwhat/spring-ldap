bce6d7e33b3335b8a84f84090def3fc56de65286
wkkim <wonkkyuu@gmail.com>
2018-03-12 09:54:53 +0900

    junit 테스트 클래스 추가
    -. ldapOuManager 테스트

--- src/test/java/local/wkim/ldap/manager/OuManagerTest.java
+++ src/test/java/local/wkim/ldap/manager/OuManagerTest.java
@@ -1,22 +1,26 @@
 package local.wkim.ldap.manager;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ldap.query.LdapQuery;
+import org.springframework.ldap.query.LdapQueryBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import local.wkim.ldap.connection.LdapConnectionProviderFactory;
 import local.wkim.ldap.connection.base.LdapConnectionProvider;
+import local.wkim.ldap.entity.LdapOu;
 import local.wkim.ldap.manager.base.LdapManager;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ManagerTest {
+public class OuManagerTest {
 
-	private static final Logger LOG = LoggerFactory.getLogger(ManagerTest.class);
+	private static final Logger LOG = LoggerFactory.getLogger(OuManagerTest.class);
 	
 	@Autowired
 	private LdapConnectionProviderFactory ldapConnectionProviderFactory;
@@ -24,24 +28,41 @@ public class ManagerTest {
 	@Autowired
 	private LdapManagerFactory ldapManagerFactory;
 	
-	@Test
+	private LdapManager<LdapOu> manager = null;
+	
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setup() {
+		manager = (LdapManager<LdapOu>) ldapManagerFactory.createLdapManager("ou");
+	}
+	
+//	@Test
 	public void create() {
 		
-		LdapManager<?> manager = ldapManagerFactory.createLdapManager("user");
-		LOG.debug("type = {}", manager.genericClassType().getName());
-		
 		LdapConnectionProvider provider = ldapConnectionProviderFactory.createConnectionProvider("propertyBase");
-		LOG.debug("provider = {}", provider.createConnection());
+		LOG.debug("`e`provider = {}", provider.createConnection());
 		
 		LOG.debug("create... !");
 	}
 	
-	@Test
+//	@Test
 	public void update() {
 		
-		LdapManager<?> manager = ldapManagerFactory.createLdapManager("ou");
-		LOG.debug("type = {}", manager.genericClassType().getName());
-		
 		LOG.debug("update... !");
+	}
+	
+	@Test
+	public void find() {
+		
+		LdapConnectionProvider provider = ldapConnectionProviderFactory.createConnectionProvider("propertyBase");
+		
+		String findOu = "wkkim";
+		LdapQuery query = LdapQueryBuilder.query()
+				.where("objectClass").is("organizationalUnit")
+				.and("ou").is(findOu);
+		LOG.debug("`e`query = {}", query.toString());
+		
+		LdapOu find = manager.connect(provider).find(query);
+		LOG.debug("`e`found = {}", find);
 	}
 }
